12d0fe7f377660ed37dbcea98b9ffd81185c07ba
TylerovaTer <98687849+TylerovaTer@users.noreply.github.com>
2022-07-19 11:07:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let navigace;
+navigace = document.querySelector("nav");
+let tlacitko;
+tlacitko = document.querySelector(".zavrit");
+
+const menu = () => {
+    navigace.classList.toggle("klik");
+    tlacitko.classList.toggle("open");
+}
+
+
+
